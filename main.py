389489a3943f56ca1eb89619b8b091a94037c335
ahmedwaelprogrammer <aw88301@gmail.com>
2025-05-06 11:42:28 +0300

    CLI Todo App v0.7

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
-
+todos = []
 while True:
     user_action = input("Type 'add' , 'show' ,  'edit' , 'complete' 'exit':").strip()
     match user_action:
         case 'add':
             todo = input("Enter a TODO:")+ "\n"
-            file = open("todos.txt" , "r")
-            todos = file.readlines()
-            file.close()
-            todos.append(todo)
+            with open("todos.txt" , "r") as file:
+                todos = file.readlines()
+                todos.append(todo)
 
-            file = open("todos.txt", "w")
-            file.writelines(todos)
-            file.close()
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
+
         case 'show':
-            file = open("todos.txt", "r")
-            todos = file.readlines()
-            file.close()
-
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
 
             for i , todo in enumerate(todos):
                 todo = todo.strip("\n")
                 print(f"{i+1} -{todo}")
         case 'edit':
             chose_todo = int(input("The number of the TODO you want to edit:")) - 1
-            new_todo = input("Type the new TODO:")
-            todos[chose_todo] = new_todo
+            new_todo = input("Type the new TODO:") + '\n'
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+                todos[chose_todo] = new_todo
+
+            with open("todos.txt" , 'w') as file:
+                file.writelines(todos)
         case 'complete':
             chose_todo = int(input("The number of the TODO you want to complete:")) - 1
-            todos.pop(chose_todo)
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+                todos.pop(chose_todo)
+
+            with open("todos.txt" , 'w') as file:
+                file.writelines(todos)
+
         case 'exit':
             break
         case _:
