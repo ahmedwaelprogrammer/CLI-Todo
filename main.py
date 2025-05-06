f76c78c94d2331b45e01e1d4b67c95924faba75f
ahmedwaelprogrammer <aw88301@gmail.com>
2025-05-06 14:01:27 +0300

    CLI Todo App v0.75

--- main.py
+++ main.py
@@ -1,43 +1,42 @@
 todos = []
 while True:
     user_action = input("Type 'add' , 'show' ,  'edit' , 'complete' 'exit':").strip()
-    match user_action:
-        case 'add':
-            todo = input("Enter a TODO:")+ "\n"
-            with open("todos.txt" , "r") as file:
-                todos = file.readlines()
-                todos.append(todo)
+    if 'add' in user_action[0:3]:
+        todo = user_action[4:].strip() + '\n'
+        with open("todos.txt" , "r") as file:
+            todos = file.readlines()
+            todos.append(todo)
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
-        case 'show':
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+    elif 'show' in user_action[0:4]:
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            for i , todo in enumerate(todos):
-                todo = todo.strip("\n")
-                print(f"{i+1} -{todo}")
-        case 'edit':
-            chose_todo = int(input("The number of the TODO you want to edit:")) - 1
-            new_todo = input("Type the new TODO:") + '\n'
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
-                todos[chose_todo] = new_todo
+        for i , todo in enumerate(todos):
+            todo = todo.strip("\n")
+            print(f"{i+1} -{todo}")
+    elif 'edit' in user_action[0:4]:
+        chose_todo = int(user_action[4:]) - 1
+        new_todo = input("Type the new TODO:") + '\n'
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+            todos[chose_todo] = new_todo
 
-            with open("todos.txt" , 'w') as file:
-                file.writelines(todos)
-        case 'complete':
-            chose_todo = int(input("The number of the TODO you want to complete:")) - 1
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
-                todos.pop(chose_todo)
+        with open("todos.txt" , 'w') as file:
+            file.writelines(todos)
+    elif 'complete' in user_action[0:8]:
+        chose_todo = int(user_action[9:]) - 1
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+            todos.pop(chose_todo)
 
-            with open("todos.txt" , 'w') as file:
-                file.writelines(todos)
+        with open("todos.txt" , 'w') as file:
+            file.writelines(todos)
 
-        case 'exit':
-            break
-        case _:
-            print("Invalid input")
+    elif 'exit' in user_action[0:4]:
+        break
+    else:
+        print("Invalid input")
 print("Bye!")
